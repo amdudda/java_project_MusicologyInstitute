43e43193608cdd274906f4bae9b05bf29f077361
amdudda <tk0654wm@go.minneapolis.edu>
2015-11-30 15:25:56 -0600

    Start of BrowseListDataModel Data model for GUI browsing views.

---
+++ src/com/amdudda/BrowseListDataModel.java
@@ -0,0 +1,66 @@
+package com.amdudda;
+
+import javax.swing.table.AbstractTableModel;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by amdudda on 11/30/15.
+ */
+public class BrowseListDataModel extends AbstractTableModel {
+    int rowcount = 0;
+    int colcount = 0;
+    ResultSet browseTable = null;
+
+    public BrowseListDataModel(ResultSet bT) {
+        this.browseTable = bT;
+    }
+
+    private void setTableDimensions() {
+        try {
+            colcount = browseTable.getMetaData().getColumnCount();
+            rowcount = countRows();
+        } catch (SQLException sqle) {
+            System.out.println("Unable to fetch table dimensions.\n" + sqle);
+        }
+
+    }
+
+    private int countRows() {
+        int tally = 0;
+        try {
+            browseTable.beforeFirst();
+            while (browseTable.next()) {
+                tally++;
+            }
+            browseTable.beforeFirst();
+        } catch (SQLException sqle) {
+            System.out.println("Unable to fetch row count.\n" + sqle);
+        }
+        return tally;
+    }
+
+    @Override
+    public int getRowCount() {
+        return this.rowcount;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return this.colcount;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Object contents;
+        try {
+            browseTable.absolute(rowIndex + 1);
+            contents = browseTable.getObject(columnIndex + 1);
+            // return an empty string if the value is null, or the datamodel won't know what to display
+            if (contents == null) contents = "";
+        } catch (SQLException sqle) {
+            System.out.println("Unable to get data for cell at row " + rowIndex + ", column " + columnIndex + ".\n" + sqle);
+        }
+        return null;
+    }
+}
