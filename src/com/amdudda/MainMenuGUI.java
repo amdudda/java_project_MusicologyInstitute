aec7db5aeb2d8546fbeb502fee846d8c3a1916ff
amdudda <tk0654wm@go.minneapolis.edu>
2015-12-01 11:59:46 -0600

    Created Main Menu GUI

---
+++ src/com/amdudda/MainMenuGUI.java
@@ -0,0 +1,14 @@
+package com.amdudda;
+
+import javax.swing.*;
+
+/**
+ * Created by amdudda on 12/1/15.
+ */
+public class MainMenuGUI {
+    private JPanel mainMenuRootPanel;
+    private JButton browseInstrumentsButton;
+    private JButton browseContactsButton;
+    private JButton addNewInstrumentButton;
+    private JButton addNewContactButton;
+}
