32660a2b619f657476b8fe9601111e3a76a0eaa7
amdudda <tk0654wm@minneapolis.edu>
2015-12-06 15:15:47 -0600

    Created UpdateContact gui form. Leaving updateinstrument alone for now - will try to fix later.

---
+++ src/com/amdudda/UpateContact.java
@@ -0,0 +1,20 @@
+package com.amdudda;
+
+import javax.swing.*;
+
+/**
+ * Created by amdudda on 12/6/15.
+ */
+public class UpateContact {
+    private JTextField contactNameTextField;
+    private JTextField businessNameTextField;
+    private JTextArea addressTextArea;
+    private JTextField cityTextField;
+    private JTextField postalCodeTextField;
+    private JComboBox stateComboBox;
+    private JComboBox countryComboBox;
+    private JTextField contactPhoneTextField;
+    private JTextField businessPhoneTextField;
+    private JComboBox contactTypeComboBox;
+    private JTextArea textArea1;
+}
