fafb9c343a87f94922c9359d32363b540433f05e
amdudda <tk0654wm@minneapolis.edu>
2015-12-09 12:36:23 -0600

    Added SQL scripts to populate test data.

---
+++ SetupFiles/SampleData/20-PopulateContacts.sql
@@ -0,0 +1,10 @@
+INSERT INTO Contact (ContactName,BusinessName,Address,City,State,PostalCode,Country,BusinessPhone,ContactPhone,ContactType,Notes)
+VALUES 
+("Suzanne Merveilleux","Musical Instrument Museum of Brussels","9 Rue Atomium",
+"Bruxelles",NULL,"10910","Belgium",NULL,NULL,"Museum",NULL),
+("YoYo Ma",NULL,"PO Box 010101","Washington","DC","10101","United States",NULL,"101-910-2222",
+"Musician","Wants to borrow our cello for SPCO concert."),
+("Anna Dudda",NULL,"4008 Fifth St NE","Columbia Heights","MN","55421","United States",NULL,"763-999-9999",
+"Donor","Donated database to the museum."),
+(NULL,"University of MN","1 Oak Street","Minneapolis","MN","55408","United States",NULL,NULL,"School",NULL)
+;
