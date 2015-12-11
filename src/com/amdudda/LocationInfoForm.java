e66ddb6fcaee6e4420e6350c162760b6c3cb8fed
amdudda <tk0654wm@minneapolis.edu>
2015-12-10 20:18:08 -0600

    Created LocationInfoForm

---
+++ src/com/amdudda/LocationInfoForm.java
@@ -0,0 +1,25 @@
+package com.amdudda;
+
+import javax.swing.*;
+
+/**
+ * Created by amdudda on 12/10/15.
+ */
+public class LocationInfoForm {
+    private JRadioButton storageOrLibraryRadioButton;
+    private JRadioButton onExhibitRadioButton;
+    private JRadioButton onLoanRadioButton;
+    private ButtonGroup currentLocationButtonGroup;
+    private JComboBox storageTypeComboBox;
+    private JTextField StartDateTextField;
+    private JTextField EndDateTextField;
+    private JComboBox LocInRmComboBox;
+    private JTextField contactIDTextField;
+    private JTextField exhibitRoomTextField;
+    private JTextField exhibitIDTextField;
+    private JTextField cabinetTextField;
+    private JTextField shelfTextField;
+    private JButton updateDatabaseButton;
+    private JButton exitDiscardChangesButton;
+    private JTextField storageRoomTextField;
+}
