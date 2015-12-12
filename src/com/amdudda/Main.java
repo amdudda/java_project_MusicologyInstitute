d73d993e41b9b2ba2ddfd68212846e4efffbe25e
amdudda <tk0654wm@minneapolis.edu>
2015-12-12 15:33:28 -0600

    Cleanup of unused imports.

--- src/com/amdudda/Main.java
+++ src/com/amdudda/Main.java
@@ -14,9 +14,6 @@ public class Main {
         // (a) dbsetup closes it, and (b) if dbFlag.txt set to true, it never actually fires up.
         MainMenuGUI mMG = new MainMenuGUI();
 
-        /*Loan loan = new Loan(7,1);
-        System.out.println((loan.toString()));*/
-
         // and close everything when the program exits - now taken care of in Main Menu screen.
         //Database.closeConnStatement();
 
